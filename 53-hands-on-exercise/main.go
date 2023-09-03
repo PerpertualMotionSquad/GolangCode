82c4d12a1b7fdc47ae84f18ea5ff9075c0ee888b
Shubham Shukla <shubhamshukla@192.168.1.11>
2023-09-03 23:24:50 +0530

    Go Code examples work

---
+++ 53-hands-on-exercise/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var a string = "10"
+var b string = "20"
+
+func main() {
+	c := "30"
+	d := "40"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
